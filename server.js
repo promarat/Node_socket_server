adf1e1361d03158c20dfc1d83b2048ff4e6f9353
prodev810 <promarat71.bs@gmail.com>
2021-10-06 17:14:35 -0300

    upgrade server

--- server.js
+++ server.js
@@ -2,17 +2,61 @@ let express = require('express');
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+const { get_Current_User, user_Disconnect, join_User } = require("./dummyuser");
 // const port = 3000;
 
-io.on("connection", socket => {
-    console.log("a user connected :D");
-    socket.on("socket_follow_user", msg => {
-        console.log(msg);
-        io.emit("socket_follow_user", msg);
+//initializing the socket io connection 
+io.on("connection", (socket) => {
+    //for a new user joining the room
+    socket.on("joinRoom", ({ username, roomname }) => {
+      //* create user
+      const p_user = join_User(socket.id, username, roomname);
+      console.log(socket.id, "=id");
+      socket.join(p_user.room);
+  
+      //display a welcome message to the user who have joined a room
+      socket.emit("message", {
+        userId: p_user.id,
+        username: p_user.username,
+        text: `Welcome ${p_user.username}`,
+      });
+  
+      //displays a joined room message to all other room users except that particular user
+      socket.broadcast.to(p_user.room).emit("message", {
+        userId: p_user.id,
+        username: p_user.username,
+        text: `${p_user.username} has joined the chat`,
+      });
     });
-});
-const PORT = process.env.PORT || 3000;
+  
+    //user sending message
+    socket.on("chat", (text) => {
+      //gets the room user and the message sent
+      const p_user = get_Current_User(socket.id);
+  
+      io.to(p_user.room).emit("message", {
+        userId: p_user.id,
+        username: p_user.username,
+        text: text,
+      });
+    });
+  
+    //when the user exits the room
+    socket.on("disconnect", () => {
+      //the user is deleted from array of users and a left room message displayed
+      const p_user = user_Disconnect(socket.id);
+  
+      if (p_user) {
+        io.to(p_user.room).emit("message", {
+          userId: p_user.id,
+          username: p_user.username,
+          text: `${p_user.username} has left the chat`,
+        });
+      }
+    });
+  });
+const PORT = process.env.PORT || 7131;
 server.listen(PORT, err => {
     if(err) throw err;
-    console.log("Server running");
+    console.log("Server running: PORT:" + PORT);
 });
